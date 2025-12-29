2063642ba00d027873f98098330a41f06bcb4638
Talha86-ux <rjtalha555@gmail.com>
2025-12-30 04:04:02 +0500

    Rescue Neon DB connection failure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_133825) do
+ActiveRecord::Schema.define(version: 2025_12_28_104329) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
